5a2c8a8ab9c4c75ac792da08d646fecac9d91b0f
thiagopessoa <thiagopessoa@sefaz.gov.pi.br>
2025-09-09 10:17:17 -0300

    Container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import heroImg from '../assets/image 533.svg'
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex items-center flex-col justify-center min-h-{calc(100vh - 80px)}">
+        <h2 className="font-medium text-2xl mb-2">Gerencie sua empresa</h2>
+        <h1 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl">Atendimentos, clientes</h1>
+        <Image alt="Imagem hero do dev controle" width={600} className="max-w-sm md:max-w-xl" src={heroImg}/>
       </main>
-    </div>
   );
 }
