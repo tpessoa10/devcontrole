5c0b13c00c6eea71f6cdab083c64eb3858c67119
thiagopessoa <thiagopessoa@sefaz.gov.pi.br>
2025-09-10 09:27:20 -0300

    Configuração de autenticação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "../components/header";
+import { AuthProvider } from "@/providers/auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Header/>
-        {children}
+        <AuthProvider>
+          <Header />
+          {children}
+        </AuthProvider>
       </body>
-    </html>
+    </html >
   );
 }
