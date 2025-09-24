61421ce5676a002ed5683e30d512b603bf96c7d0
Thiago Pessoa <tpessoa96@gmail.com>
2025-09-24 08:58:42 -0300

    Deletando clientes

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -1,21 +1,21 @@
-import { NextResponse} from 'next/server'
+import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
-import prismaClient  from '@/lib/prisma'
+import prismaClient from '@/lib/prisma'
 
 
 export async function POST(request: Request) {
     const session = await getServerSession(authOptions)
 
-    if(!session || !session.user){
-        return NextResponse.json({error:"NOT AUTHORIZED"}, {status:401})
+    if (!session || !session.user) {
+        return NextResponse.json({ error: "NOT AUTHORIZED" }, { status: 401 })
     }
 
-    const { name, email, phone, address, userId} = await request.json()
+    const { name, email, phone, address, userId } = await request.json()
 
-    try{
+    try {
         await prismaClient.customer.create({
-            data:{
+            data: {
                 name,
                 phone,
                 email,
@@ -24,9 +24,38 @@ export async function POST(request: Request) {
             }
         })
 
-        return NextResponse.json({ message: "Cliente cadastrado com sucesso"})
-    } catch(err){
-        return NextResponse.json({error:"Failed create new customer"}, { status:400})
+        return NextResponse.json({ message: "Cliente cadastrado com sucesso" })
+    } catch (err) {
+        return NextResponse.json({ error: "Failed create new customer" }, { status: 400 })
+    }
+
+}
+
+export async function DELETE(request: Request) {
+    const session = await getServerSession(authOptions)
+
+    if (!session || !session.user) {
+        return NextResponse.json({ error: "NOT AUTHORIZED" }, { status: 401 })
+    }
+
+    const { searchParams } = new URL(request.url)
+    const userId = searchParams.get("id")
+
+    if(!userId){
+        return NextResponse.json({ error: "Failed delete customer" }, { status: 400 })
+    }
+
+    try {
+        await prismaClient.customer.delete({
+            where:{
+                id:userId as string
+            }
+        })
+
+        return NextResponse.json({ message: 'Cliente deletado com sucesso!'})
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ error: "Failed delete customer" }, { status: 400 })
     }
 
 }
--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,13 +1,29 @@
+"use client"
+
+import { api } from '@/lib/api'
 import { CustomerProps } from '@/utils/customer.type'
+import { id } from 'zod/locales'
 
 export function CardCustomer({customer}: {customer: CustomerProps}){
-    console.log('custi ', customer)
+    
+    async function handleDeleteCustomer() {
+        try{
+            const response = await api.delete("/api/customer", {
+            params:{
+                id: customer.id
+            }
+        })
+        } catch(err){
+            console.log(err)
+        }
+    }
+
     return(
         <article className="flex flex-col bg-gray-100 border-2 p-2 rounded-lg gap-2 hover:slace-105 duration-300">
             <h2><a className="font-bold" href="">Nome</a>: {customer?.name}</h2>
             <p className="font-bold" >Email: {customer?.email}</p>
             <p className="font-bold" >Telefone: {customer?.phone}</p>
-            <button className="bg-red-500 px-4 rounded text-white mt-2 self-start">
+            <button onClick={handleDeleteCustomer} className="bg-red-500 px-4 rounded text-white mt-2 self-start">
                 Deletar
             </button>
         </article>    
