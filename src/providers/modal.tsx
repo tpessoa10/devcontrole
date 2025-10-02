1c3e753e7b19df99b6228adae2d4dbf3a5a5a52f
Thiago Pessoa <tpessoa96@gmail.com>
2025-10-02 09:28:55 -0300

    Modal

--- src/providers/modal.tsx
+++ src/providers/modal.tsx
@@ -3,22 +3,30 @@
 import { createContext, ReactNode, useState} from 'react'
 import { TicketProps } from '@/utils/ticket.type'
 import { ModalTicket } from '@/components/modal';
+import { CustomerProps } from '@/utils/customer.type';
 
 interface ModalContextData{
     visible: boolean;
     handleModalVisible:() => void;
+    ticket: TicketInfo | undefined;
+}
+
+interface TicketInfo{
+    ticket:TicketProps;
+    customer:CustomerProps;
 }
 
 export const ModalContext = createContext({} as ModalContextData)
 
 export const ModalProvider = ({children}: {children:ReactNode}) => {
     const [visible, setVisible] = useState(false)
+    const [ticket, setTicker] = useState<TicketInfo>()
 
     function handleModalVisible(){
         setVisible(!visible)
     }
     return(
-        <ModalContext.Provider value={{ visible, handleModalVisible}}>
+        <ModalContext.Provider value={{ visible, handleModalVisible, ticket}}>
             {visible && (
                 <ModalTicket/>
             )}
